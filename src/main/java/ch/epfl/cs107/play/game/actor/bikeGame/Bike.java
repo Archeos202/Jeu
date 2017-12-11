0b214960dbb04f8c3980f60fd46bc67845259467
Michaël-PC\Michaël <michael.tasev@gmail.com>
2017-12-11 09:10:08 +0100

    Bras levés et pédales

--- src/main/java/ch/epfl/cs107/play/game/actor/bikeGame/Bike.java
+++ src/main/java/ch/epfl/cs107/play/game/actor/bikeGame/Bike.java
@@ -20,12 +20,14 @@ import ch.epfl.cs107.play.math.Transform;
 import ch.epfl.cs107.play.math.Vector;
 import ch.epfl.cs107.play.window.Canvas;
 
-public class Bike extends GameEntity implements Actor  {
+public class Bike extends GameEntity implements Actor {
 	private ShapeGraphics graphics;
 	private ShapeGraphics headGraphics;
-	private ShapeGraphics armGraphics;
+	private ShapeGraphics leftArmGraphics;
+	private ShapeGraphics rightArmGraphics;
 	private ShapeGraphics backGraphics;
-	private ShapeGraphics thighGraphics;
+	private ShapeGraphics leftThighGraphics;
+	private ShapeGraphics rightThighGraphics;
 	private ShapeGraphics leftFootGraphics;
 	private ShapeGraphics rightFootGraphics;
 	private Wheel rightWheel;
@@ -33,75 +35,73 @@ public class Bike extends GameEntity implements Actor  {
 	private boolean regard;
 	private boolean hit;
 	private float MAX_WHEEL_SPEED;
-	
-	//MAX_WHEEL_SPEED et le boolean pour le regard
+
+	// MAX_WHEEL_SPEED et le boolean pour le regard
 	// setviewcandidate ????
-	
+
 	public Bike(ActorGame game, Vector position) {
 		super(game, false, position);
-		PartBuilder partBuilder = getEntity().createPartBuilder(); 
-        
-        Polygon polygon = new Polygon(
-        		new Vector(0.0f, 0.5f),
-        		new Vector(0.5f, 1.0f),
-        		new Vector(0.0f, 2.0f),
-        		new Vector(-0.5f, 1.0f) ); 
-        partBuilder.setGhost(true);
-        partBuilder.setShape(polygon);
-        partBuilder.setCollisionGroup(2);
-        partBuilder.build();
-		
-        MAX_WHEEL_SPEED = 20.0f;
-        regard = true;
-        
-        BikerGraphics();
-        
-    	graphics = new ShapeGraphics(polygon, Color.ORANGE, Color.RED, 0.1f);
-        graphics.setParent(getEntity());
-        
-        leftWheel = new Wheel(game, false, new Vector (3.0f, 5.0f),0.5f, "explosive.11.png", 1);
-		rightWheel = new Wheel(game, false, new Vector (5.0f, 5.0f),0.5f, "explosive.11.png", 1);
-        leftWheel.attach(getEntity(), new Vector(-1.0f, 0.0f), new Vector(-0.5f, -1.0f)); 
+		PartBuilder partBuilder = getEntity().createPartBuilder();
+
+		Polygon polygon = new Polygon(new Vector(0.0f, 0.5f), new Vector(0.5f, 1.0f), new Vector(0.0f, 2.0f),
+				new Vector(-0.5f, 1.0f));
+		partBuilder.setGhost(true);
+		partBuilder.setShape(polygon);
+		partBuilder.setCollisionGroup(2);
+		partBuilder.build();
+
+		MAX_WHEEL_SPEED = 20.0f;
+		regard = true;
+
+		BikerGraphics();
+
+		graphics = new ShapeGraphics(polygon, Color.ORANGE, Color.RED, 0.1f);
+		graphics.setParent(getEntity());
+
+		leftWheel = new Wheel(game, false, new Vector(3.0f, 5.0f), 0.5f, "explosive.11.png", 1);
+		rightWheel = new Wheel(game, false, new Vector(5.0f, 5.0f), 0.5f, "explosive.11.png", 1);
+		leftWheel.attach(getEntity(), new Vector(-1.0f, 0.0f), new Vector(-0.5f, -1.0f));
 		rightWheel.attach(getEntity(), new Vector(1.0f, 0.0f), new Vector(0.5f, -1.0f));
-		
+
 		ContactListener listener = new ContactListener() {
-			@Override 
+			@Override
 			public void beginContact(Contact contact) {
-				if (contact.getOther().isGhost()) 
-					return; 
-				if (contact.getOther().getCollisionGroup()==1) {
+				if (contact.getOther().isGhost())
+					return;
+				if (contact.getOther().getCollisionGroup() == 1) {
 					System.out.println("roue");
 					return;
 				}
-				hit = true; 
-				} 
-			@Override 
+				hit = true;
+			}
+
+			@Override
 			public void endContact(Contact contact) {
 				hit = false;
-			} 
-				};
+			}
+		};
 		getEntity().addContactListener(listener);
 	}
-	
+
 	public void update(float deltaTime) {
 		if (getOwner().getKeyboard().get(KeyEvent.VK_SPACE).isPressed()) {
-			regard = !regard ;
+			regard = !regard;
 			BikerGraphics();
-			}
+		}
 		rightWheel.relax();
 		leftWheel.relax();
 		if (getOwner().getKeyboard().get(KeyEvent.VK_DOWN).isDown()) {
 			leftWheel.power(0.0f);
 			rightWheel.power(0.0f);
 		}
-		if ((regard) && (leftWheel.getSpeed()>= -MAX_WHEEL_SPEED)) {
+		if ((regard) && (leftWheel.getSpeed() >= -MAX_WHEEL_SPEED)) {
 			if (getOwner().getKeyboard().get(KeyEvent.VK_UP).isDown()) {
-			leftWheel.power(-MAX_WHEEL_SPEED);
+				leftWheel.power(-MAX_WHEEL_SPEED);
 			}
 		}
-		if ((!regard) && (rightWheel.getSpeed()<= MAX_WHEEL_SPEED)) {
+		if ((!regard) && (rightWheel.getSpeed() <= MAX_WHEEL_SPEED)) {
 			if (getOwner().getKeyboard().get(KeyEvent.VK_UP).isDown()) {
-			rightWheel.power(MAX_WHEEL_SPEED);
+				rightWheel.power(MAX_WHEEL_SPEED);
 			}
 		}
 		if (getOwner().getKeyboard().get(KeyEvent.VK_LEFT).isDown()) {
@@ -110,97 +110,161 @@ public class Bike extends GameEntity implements Actor  {
 		if (getOwner().getKeyboard().get(KeyEvent.VK_RIGHT).isDown()) {
 			getEntity().applyAngularForce(-30.0f);
 		}
+		pedal(deltaTime);
+
 	}
+
 	private void BikerGraphics() {
 		Circle head = new Circle(0.2f, getHeadLocation());
-		Polyline  arm = new Polyline (
-				getShoulderLocation(),
-				getHandLocation());
-		Polyline back = new Polyline (
-				getShoulderLocation(),
-				getBottomLocation());
-		Polyline  thigh = new Polyline (
-				getBottomLocation(),
-				getKneeLocation());
-		Polyline  leftFoot = new Polyline (
-				getKneeLocation(),
-				getLeftFootLocation());
-		Polyline  rightFoot = new Polyline (
-				getKneeLocation(),
-				getRightFootLocation());
-		
-		headGraphics = new ShapeGraphics(head,Color.WHITE, Color.WHITE, 0.2f);
-		armGraphics = new ShapeGraphics(arm,Color.WHITE, Color.WHITE, 0.2f);
-    	backGraphics = new ShapeGraphics(back,Color.WHITE, Color.WHITE, 0.2f);
-    	thighGraphics = new ShapeGraphics(thigh,Color.WHITE, Color.WHITE, 0.2f);
-    	leftFootGraphics = new ShapeGraphics(leftFoot,Color.WHITE, Color.WHITE, 0.2f);
-    	rightFootGraphics = new ShapeGraphics(rightFoot,Color.WHITE, Color.WHITE, 0.2f);
-    	
-    	headGraphics.setParent(getEntity());
-    	armGraphics.setParent(getEntity());
-        backGraphics.setParent(getEntity());
-        thighGraphics.setParent(getEntity());
-        leftFootGraphics.setParent(getEntity());
-        rightFootGraphics.setParent(getEntity());
+		Polyline leftArm = new Polyline(getShoulderLocation(), getLeftHandLocation());
+		Polyline rightArm = new Polyline(getShoulderLocation(), getRightHandLocation());
+		Polyline back = new Polyline(getShoulderLocation(), getBottomLocation());
+		Polyline leftThigh = new Polyline(getBottomLocation(), getLeftKneeLocation());
+		Polyline rightThigh = new Polyline(getBottomLocation(), getRightKneeLocation());
+		Polyline leftFoot = new Polyline(getLeftKneeLocation(), getLeftFootLocation());
+		Polyline rightFoot = new Polyline(getRightKneeLocation(), getRightFootLocation());
+
+		headGraphics = new ShapeGraphics(head, Color.WHITE, Color.WHITE, 0.15f);
+		leftArmGraphics = new ShapeGraphics(leftArm, Color.WHITE, Color.WHITE, 0.15f);
+		rightArmGraphics = new ShapeGraphics(rightArm, Color.WHITE, Color.WHITE, 0.15f);
+		backGraphics = new ShapeGraphics(back, Color.WHITE, Color.WHITE, 0.15f);
+		leftThighGraphics = new ShapeGraphics(leftThigh, Color.BLUE, Color.BLUE, 0.15f);
+		rightThighGraphics = new ShapeGraphics(rightThigh, Color.RED, Color.RED, 0.15f);
+		leftFootGraphics = new ShapeGraphics(leftFoot, Color.BLUE, Color.BLUE, 0.15f);
+		rightFootGraphics = new ShapeGraphics(rightFoot, Color.RED, Color.RED, 0.15f);
+
+		headGraphics.setParent(getEntity());
+		leftArmGraphics.setParent(getEntity());
+		rightArmGraphics.setParent(getEntity());
+		backGraphics.setParent(getEntity());
+		leftThighGraphics.setParent(getEntity());
+		rightThighGraphics.setParent(getEntity());
+		leftFootGraphics.setParent(getEntity());
+		rightFootGraphics.setParent(getEntity());
 	}
-	
+
 	public void deleteGraphics() {
 		Circle circle = new Circle(0.0f);
-		headGraphics = new ShapeGraphics(circle,Color.WHITE, Color.WHITE, 0.2f);
-		armGraphics = new ShapeGraphics(circle,Color.WHITE, Color.WHITE, 0.2f);
-		backGraphics = new ShapeGraphics(circle,Color.WHITE, Color.WHITE, 0.2f);
-		thighGraphics = new ShapeGraphics(circle,Color.WHITE, Color.WHITE, 0.2f);
-		leftFootGraphics = new ShapeGraphics(circle,Color.WHITE, Color.WHITE, 0.2f);
-		rightFootGraphics = new ShapeGraphics(circle,Color.WHITE, Color.WHITE, 0.2f);
+		headGraphics = new ShapeGraphics(circle, Color.WHITE, Color.WHITE, 0.2f);
+		leftArmGraphics = new ShapeGraphics(circle, Color.WHITE, Color.WHITE, 0.2f);
+		rightArmGraphics = new ShapeGraphics(circle, Color.WHITE, Color.WHITE, 0.2f);
+		backGraphics = new ShapeGraphics(circle, Color.WHITE, Color.WHITE, 0.2f);
+		leftThighGraphics = new ShapeGraphics(circle, Color.WHITE, Color.WHITE, 0.2f);
+		rightThighGraphics = new ShapeGraphics(circle, Color.WHITE, Color.WHITE, 0.2f);
+		leftFootGraphics = new ShapeGraphics(circle, Color.WHITE, Color.WHITE, 0.2f);
+		rightFootGraphics = new ShapeGraphics(circle, Color.WHITE, Color.WHITE, 0.2f);
 		graphics = new ShapeGraphics(circle, Color.ORANGE, Color.RED, 0.0f);
 	}
-	
+
 	private Vector getHeadLocation() {
 		return new Vector(0.0f, 1.75f);
 	}
+
 	private Vector getShoulderLocation() {
 		return new Vector(0.0f, 1.55f);
 	}
-	private Vector getHandLocation() {
+
+	private Vector getLeftHandLocation() {
 		if (regard) {
 			return new Vector(0.5f, 1.0f);
-		}
-		else {
+		} else {
 			return new Vector(-0.5f, 1.0f);
 		}
 	}
+
+	private Vector getRightHandLocation() {
+		if (regard) {
+			return new Vector(0.5f, 1.0f);
+		} else {
+			return new Vector(-0.5f, 1.0f);
+		}
+	}
+
+	private Vector getVictoryLeftHandLocation() {
+		return new Vector(0.5f, 2.1f);
+	}
+
+	private Vector getVictoryRightHandLocation() {
+		return new Vector(-0.5f, 2.1f);
+	}
+
 	private Vector getBottomLocation() {
 		if (regard) {
 			return new Vector(-0.5f, 1.0f);
-		}
-		else { 
-			return new Vector (0.5f, 1.0f);
+		} else {
+			return new Vector(0.5f, 1.0f);
 		}
 	}
-	private Vector getKneeLocation() {
-			return new Vector(0.0f, 0.6f);
+
+	private Vector getLeftKneeLocation() {
+		return new Vector(0.0f, 0.6f);
 	}
+
+	private Vector getRightKneeLocation() {
+		return new Vector(0.0f, 0.6f);
+	}
+
+	private Vector getUpdateLeftKneeLocation(float deltaTime) {
+		if (regard) {
+			return new Vector(0.2f * ((float) Math.sin((double) leftWheel.getEntity().getAngularPosition())),
+					0.6f + (0.2f * ((float) Math.sin((double) leftWheel.getEntity().getAngularPosition()))));
+		} else {
+			return new Vector(-0.2f * ((float) Math.sin((double) rightWheel.getEntity().getAngularPosition())),
+					0.6f + (0.2f * ((float) Math.sin((double) rightWheel.getEntity().getAngularPosition()))));
+		}
+	}
+
+	private Vector getUpdateRightKneeLocation(float deltaTime) {
+		if (regard) {
+			return new Vector(0.2f * (-((float) Math.sin((double) leftWheel.getEntity().getAngularPosition()))),
+					0.6f + (0.2f * (-((float) Math.sin((double) leftWheel.getEntity().getAngularPosition())))));
+		} else {
+			return new Vector(-0.2f * (-((float) Math.sin((double) rightWheel.getEntity().getAngularPosition()))),
+					0.6f + (0.2f * (-((float) Math.sin((double) rightWheel.getEntity().getAngularPosition())))));
+		}
+	}
+
 	private Vector getLeftFootLocation() {
 		if (regard) {
 			return new Vector(-0.3f, 0.0f);
-		}
-		else { 
-			return new Vector (0.3f, 0.0f);
+		} else {
+			return new Vector(0.3f, 0.0f);
 		}
 	}
+
 	private Vector getRightFootLocation() {
 		if (regard) {
 			return new Vector(0.3f, 0.0f);
-		}
-		else { 
-			return new Vector (-0.3f, 0.0f);
+		} else {
+			return new Vector(-0.3f, 0.0f);
 		}
 	}
-	
+
+	private Vector getUpdateLeftFootLocation(float deltaTime) {
+		if (regard) {
+			return new Vector((float) (0.3f * Math.cos((double) leftWheel.getEntity().getAngularPosition())),
+					(float) (0.3f * Math.sin((double) leftWheel.getEntity().getAngularPosition())));
+		} else {
+			return new Vector((float) (0.3f * Math.cos((double) rightWheel.getEntity().getAngularPosition())),
+					(float) (0.3f * Math.sin((double) rightWheel.getEntity().getAngularPosition())));
+		}
+	}
+
+	private Vector getUpdateRightFootLocation(float deltaTime) {
+		if (regard) {
+			return new Vector((float) (-0.3f * Math.cos((double) leftWheel.getEntity().getAngularPosition())),
+					(float) (-0.3f * Math.sin((double) leftWheel.getEntity().getAngularPosition())));
+		} else {
+			return new Vector((float) (-0.3f * Math.cos((double) rightWheel.getEntity().getAngularPosition())),
+					(float) (-0.3f * Math.sin((double) rightWheel.getEntity().getAngularPosition())));
+		}
+	}
+
 	public boolean getHit() {
 		return hit;
 	}
-	//supprimer des actors ???
+
+	// supprimer des actors ???
 	@Override
 	public void destroy() {
 		getEntity().destroy();
@@ -208,7 +272,7 @@ public class Bike extends GameEntity implements Actor  {
 		rightWheel.destroy();
 		deleteGraphics();
 	}
-	
+
 	@Override
 	public Transform getTransform() {
 		return getEntity().getTransform();
@@ -223,13 +287,39 @@ public class Bike extends GameEntity implements Actor  {
 	public void draw(Canvas canvas) {
 		graphics.draw(canvas);
 		headGraphics.draw(canvas);
-        armGraphics.draw(canvas);
-        backGraphics.draw(canvas);
-        thighGraphics.draw(canvas);
-        leftFootGraphics.draw(canvas);
-        rightFootGraphics.draw(canvas);
-        rightWheel.draw(canvas);
-        leftWheel.draw(canvas);
+		leftArmGraphics.draw(canvas);
+		rightArmGraphics.draw(canvas);
+		backGraphics.draw(canvas);
+		leftThighGraphics.draw(canvas);
+		rightThighGraphics.draw(canvas);
+		leftFootGraphics.draw(canvas);
+		rightFootGraphics.draw(canvas);
+		rightWheel.draw(canvas);
+		leftWheel.draw(canvas);
 	}
 
+	public void victoryArms() {
+		Polyline leftArm = new Polyline(getShoulderLocation(), getVictoryLeftHandLocation());
+		Polyline rightArm = new Polyline(getShoulderLocation(), getVictoryRightHandLocation());
+		leftArmGraphics = new ShapeGraphics(leftArm, Color.WHITE, Color.WHITE, 0.15f);
+		rightArmGraphics = new ShapeGraphics(rightArm, Color.WHITE, Color.WHITE, 0.15f);
+		leftArmGraphics.setParent(getEntity());
+		rightArmGraphics.setParent(getEntity());
+	}
+
+	public void pedal(float deltaTime) {
+		Polyline leftFoot = new Polyline(getUpdateLeftKneeLocation(deltaTime), getUpdateLeftFootLocation(deltaTime));
+		Polyline rightFoot = new Polyline(getUpdateRightKneeLocation(deltaTime), getUpdateRightFootLocation(deltaTime));
+		Polyline leftThigh = new Polyline(getBottomLocation(), getUpdateLeftKneeLocation(deltaTime));
+		Polyline rightThigh = new Polyline(getBottomLocation(), getUpdateRightKneeLocation(deltaTime));
+		leftThighGraphics = new ShapeGraphics(leftThigh, Color.BLUE, Color.BLUE, 0.15f);
+		rightThighGraphics = new ShapeGraphics(rightThigh, Color.RED, Color.RED, 0.15f);
+		leftThighGraphics.setParent(getEntity());
+		rightThighGraphics.setParent(getEntity());
+		leftFootGraphics = new ShapeGraphics(leftFoot, Color.BLUE, Color.BLUE, 0.15f);
+		rightFootGraphics = new ShapeGraphics(rightFoot, Color.RED, Color.RED, 0.15f);
+		leftFootGraphics.setParent(getEntity());
+		rightFootGraphics.setParent(getEntity());
+
+	}
 }
--- src/main/java/ch/epfl/cs107/play/game/actor/bikeGame/BikeGame.java
+++ src/main/java/ch/epfl/cs107/play/game/actor/bikeGame/BikeGame.java
@@ -19,38 +19,39 @@ import ch.epfl.cs107.play.window.Window;
 
 public class BikeGame extends ActorGame {
 
-	
 	private Finish finish;
 	private Bike bike;
 	private TextGraphics message;
 	private boolean collision;
 	private boolean victoire;
-	
+
 	public boolean begin(Window window, FileSystem fileSystem) {
 		super.begin(window, fileSystem);
-		Terrain terrain = new Terrain(this, new Vector(0f , 0f) );
+		Terrain terrain = new Terrain(this, new Vector(0f, 0f));
 		addActor(terrain);
-		Crate crate1 = new Crate(this , false, new Vector(0.0f ,5.0f), 1, 1, "crate.1.png");
+		Crate crate1 = new Crate(this, false, new Vector(0.0f, 5.0f), 1, 1, "crate.1.png");
 		addActor(crate1);
-		Crate crate2 = new Crate(this , false, new Vector(0.2f ,7.0f), 1, 1, "crate.1.png");
+		Crate crate2 = new Crate(this, false, new Vector(0.2f, 7.0f), 1, 1, "crate.1.png");
 		addActor(crate2);
-		Crate crate3 = new Crate(this , false, new Vector(2.0f ,6.0f), 1, 1, "crate.1.png");
+		Crate crate3 = new Crate(this, false, new Vector(2.0f, 6.0f), 1, 1, "crate.1.png");
 		addActor(crate3);
 		bike = new Bike(this, new Vector(4.0f, 5.0f));
 		addActor(bike);
 		setViewCandidate(bike);
-		//ActorGame.setViewCandidate(bike); ??????????
+		// ActorGame.setViewCandidate(bike); ??????????
 		finish = new Finish(this, new Vector(15.0f, 3.0f));
 		addActor(finish);
-		
+
 		collision = false;
 		victoire = false;
-		message = new TextGraphics("JOUJ", 0.3f, Color.RED, Color.WHITE, 0.02f, true, false, new Vector(0.5f, 0.5f), 1.0f, 100.0f);
-		message.setParent(getCanvas()); 
+		message = new TextGraphics("", 0.3f, Color.RED, Color.WHITE, 0.02f, true, false, new Vector(0.5f, 0.5f), 1.0f,
+				100.0f);
+		message.setParent(getCanvas());
 		message.setRelativeTransform(Transform.I.translated(0.0f, -1.0f));
-		
+
 		return true;
 	}
+
 	public void update(float deltaTime) {
 		super.update(deltaTime);
 		if (bike.getHit()) {
@@ -60,10 +61,14 @@ public class BikeGame extends ActorGame {
 		if (getKeyboard().get(KeyEvent.VK_R).isPressed()) {
 			this.end();
 			this.begin(getWindow(), getFileSystem());
+		}
+		for (int i = 0; i < ActorList.size(); i++) {
+			if (!((GameEntity) ActorList.get(i)).getEntity().isAlive()) {
+				deleteActor(ActorList.get(i));
 			}
-		
+		}
 		if (collision) {
-			message.setText("sa fé tro mal");
+			message.setText("sa fé tré mal");
 			message.draw(getCanvas());
 			bike.destroy();
 			deleteActor(bike);
@@ -71,26 +76,17 @@ public class BikeGame extends ActorGame {
 		if (finish.getHit()) {
 			victoire = true;
 		}
-		if (victoire){
+		if (victoire) {
 			message.setText("c gagner");
 			message.draw(getCanvas());
-		}
-		if (getKeyboard().get(KeyEvent.VK_R).isPressed()) {
-				this.end();
-				this.begin(getWindow(), getFileSystem());
-		} 
-		for(int i = 0; i < ActorList.size(); i++) {
-			if(!((GameEntity)ActorList.get(i)).getEntity().isAlive()) {
-				deleteActor(ActorList.get(i));
-			}
+			bike.victoryArms();
+
 		}
 	}
-	
+
 	public void end() {
 		for (Actor actor : ActorList) {
 			actor.destroy();
 		}
-		
-		}
 	}
-
+}
